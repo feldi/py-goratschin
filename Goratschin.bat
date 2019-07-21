87bb05a080ecd9f9861c72005ac047f7140a6aa7
feldi <peter.feldtmann@gmail.com>
2019-07-21 11:20:08 +0200

    fix BAT file; add logo

--- Goratschin.bat
+++ Goratschin.bat
@@ -1,6 +1,6 @@
 @echo off
 rem cd D:/chess/py-goratschin
-python goratschinLauncher.py -log gChess.log
+python goratschinLauncher.py %*
 
 rem verbose:
 rem python goratschinLauncher.py -log goratschinChess.log -v
